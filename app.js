98e9499c3d3fa3b4bf763eb9d8e78b9326dcfbdc
snowkirin <jungjinsoo10@gmail.com>
2021-11-13 12:00:18 +0900

    Hello

--- app.js
+++ app.js
@@ -1,61 +1,57 @@
-// This example shows basic use of home tabs
-// It uses an enabled app home and the app_home_opened event
-// Require the Bolt package (github.com/slackapi/bolt)
-const { App } = require("@slack/bolt");
+const { App } = require('@slack/bolt');
 
-const SLACK_BOT_TOKEN="xoxb-2317231393845-2721713159922-af6L5wbdjEQ7SxWkBwTdwoBs";
-const SLACK_SIGNING_SECRET="e4cb1b4bd9183b8a5bed69846dab4154";
-const SLACK_APP_TOKEN="xapp-1-A02M4MJETHT-2721718644819-5975b9899b4d045ba2a34f6b04e0dcac04d33b48fda00439bcdca0582e65cbdd";
+const SLACK_BOT_TOKEN="xoxb-2317231393845-2707095521495-XpA8eHz5ybNyyV77fJ16fUpk"
+const SLACK_SIGNING_SECRET="d3048e7e6ecfd6b2679adabc0f7f5851"
+const SLACK_APP_TOKEN="xapp-1-A02LT2LECUX-2723989610612-ab5ba6817161ad1b2ba65b86c92f16a9f2044f0f746590320ef3c8c3fd2957de"
 
+/* 
+This sample slack application uses SocketMode
+For the companion getting started setup guide, 
+see: https://slack.dev/bolt-js/tutorial/getting-started 
+*/
+
+// Initializes your app with your bot token and app token
 const app = new App({
     token: SLACK_BOT_TOKEN,
-    signingSecret: SLACK_SIGNING_SECRET
-  });
-// Message listener function called for messages containing "hello"
+    signingSecret: SLACK_SIGNING_SECRET,
+    socketMode: true, // add this
+    appToken: SLACK_APP_TOKEN // add this
+});
+
+// Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
-    await say({
-      "text": `👋 Hey there <@${message.user}>`,
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-            "text": `👋 Hey there <@${message.user}>`
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${message.user}>!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
           },
-          "accessory": {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "text": "Click Me",
-              "emoji": true
-            },
-            "action_id": "click_me_button"
-          }
+          "action_id": "button_click"
         }
-      ]
-    });
+      }
+    ],
+    text: `Hey there <@${message.user}>!`
   });
-  
-  // Action listener function called when an interactive component with action_id of “click_me_button” is triggered
-  app.action('click_me_button', async ({ ack, body, client, say }) => {
-    // Acknowledge action request before anything else
-    await ack();
-    
-    let channelID = body.channel.id
-    let userID = body.user.id
-    
-    // Respond to action with an ephemeral message
-    await client.chat.postEphemeral({
-      channel: channelID,
-      user: userID,
-      text: `<@${userID}> clicked the button! 🎉`
-    });
-  });
-  
-  (async () => {
-    // Start your app
-    await app.start(process.env.PORT || 3000);
-  
-    console.log('⚡️ Bolt app is running!');
-  })();
-  
+});
+
+app.action('button_click', async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
